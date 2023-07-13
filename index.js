5587cf419317b2e89238d740eff4af5598ef0329
alexvcodesphere <132889147+alexvcodesphere@users.noreply.github.com>
2023-07-13 16:32:21 +0200

    added SQLite database, ability to upload sendgrid csv exports to the database and send sendgrid templates via template id to all contacts in the dabase

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { getContactsLink, downloadAndDecompress, sendEmails } = require('./server/api');
+const { sendEmails } = require('./server/api');
 const { processContacts } = require('./server/utils');
 const app = express();
 const port = 3000;
+const sqlite3 = require('sqlite3').verbose();
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const { uploadToDb } = require('./server/uploadToDb');
 
 app.use(express.json());
 
@@ -11,32 +15,33 @@ app.all("*", (req, res, next) => {
   next();
 });
 
-app.get("/contacts", async (req, res) => {
 
-});
-
-app.post("/contacts", async (req,res) => {
+app.post("/contacts", async (req, res) => {
   console.log(req.body);
 
-  const {listId , emailText} = req.body;
-  console.log(listId);
+  const { templateId } = req.body;
 
   try {
-    const contactsUrl = await getContactsLink(listId);
-    await downloadAndDecompress(contactsUrl);
-    const emailGroups = await processContacts(2);
+    const emailGroups = await processContacts(99);
 
     // Send emails to each group
     for (let group of emailGroups) {
-      const emailAddresses = group.map(contact => contact.email); // Assuming each contact object has an 'email' field
-      await sendEmails(emailAddresses, emailText);
+        await sendEmails(group, templateId);
     }
 
     res.json(emailGroups);
   } catch (error) {
     res.status(500).json({ error: "An error occurred while fetching contacts" });
   }
-})
+});
+
+app.post('/upload', upload.single('contacts-upload'), (req, res) => {
+
+  uploadToDb(req.file.path);
+
+  res.send('File uploaded successfully');
+
+});
 
 app.use(express.static('public'));
 
--- server/utils.js
+++ server/utils.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const ndjson = require('ndjson');
+const sqlite3 = require('sqlite3').verbose();
 
 function chunkArray(array, chunkSize) {
     let results = [];
@@ -15,23 +16,27 @@ let emailGroups = [];
 
 function processContacts(chunkSize) {
     return new Promise((resolve, reject) => {
-      let data = [];
 
-      console.log("creating array from JSON")
-  
-      fs.createReadStream('server/data/contacts.json')
-        .pipe(ndjson.parse())
-        .on('data', function (obj) {
-          data.push(obj);
-        })
-        .on('end', function () {
-          emailGroups = chunkArray(data, chunkSize);
-          console.log("new data array", emailGroups);
-          resolve(emailGroups); // Resolve the promise with emailGroups
-        })
-        .on('error', function (err) {
-          reject(err); // Reject the promise if an error occurs
-        });
+      const db = new sqlite3.Database('./server/data/contacts.db', sqlite3.OPEN_READWRITE, (err) => {
+        if (err) {
+          console.error(err.message);
+          reject(err);
+        } else {
+          console.log('Connected to the database.');
+          const sql = `SELECT email FROM contacts`;
+
+          db.all(sql, [], (err, rows) => {
+            if (err) {
+              console.error(err.message);
+              reject(err);
+            } else {
+              const emailGroups = chunkArray(rows.map(row => row.email), chunkSize);
+              console.log("new data array", emailGroups);
+              resolve(emailGroups); // Resolve the promise with emailGroups
+            }
+          });
+        }
+      });
     });
   }
 
